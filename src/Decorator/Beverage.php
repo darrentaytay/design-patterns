99bbb59999cf9717669029f61395b8946dabdccf
Darren Taylor <shout@darrenonthe.net>
2017-06-08 18:38:05 +0000

    Apply fixes from StyleCI

--- src/Decorator/Beverage.php
+++ src/Decorator/Beverage.php
@@ -2,15 +2,14 @@
 
 namespace App\Decorator;
 
-abstract class Beverage {
-
-    protected $description = "Unknown beverage";
+abstract class Beverage
+{
+    protected $description = 'Unknown beverage';
 
     public function getdescription()
     {
         return $this->description;
     }
 
-    public abstract function cost();
-
+    abstract public function cost();
 }
--- src/Decorator/Decorators/CondimentDecorator.php
+++ src/Decorator/Decorators/CondimentDecorator.php
@@ -4,13 +4,12 @@ namespace App\Decorator\Decorators;
 
 use App\Decorator\Beverage;
 
-abstract class CondimentDecorator extends Beverage {
-
+abstract class CondimentDecorator extends Beverage
+{
     protected $beverage;
 
     public function __construct(Beverage $beverage)
     {
         $this->beverage = $beverage;
     }
-
 }
